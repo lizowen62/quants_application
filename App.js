a09caca240d7a4f111428d3801e41efac5eb2d9f
gabriel <motta.gabriel51@gmail.com>
2022-02-24 14:04:11 +0100

    optimizing cryptolist

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
-import { backgroundColor, borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
-import CryptoItem from './components/CryptoItem';
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from "react-native";
+import {
+  backgroundColor,
+  borderColor,
+} from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import CryptoItem from "./components/CryptoItem";
+
+import WS from "react-native-websocket";
 
 const DATA = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
   },
 ];
 
@@ -25,24 +37,68 @@ const Item = ({ title }) => (
 );
 
 const App = () => {
-  const renderItem = ({ item }) => (
-    <Item title={item.title} />
-  );
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [crypto, setCrypto] = useState([
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Thirddyuou Itiem",
+    },
+  ]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const selectCrypto = (key) => {
+    setCrypto((prev) => {
+      return prev.filter((todo) => todo.title != key);
+    });
+  };
+
+  const fetchData = async () => {
+    const resp = await fetch(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=200&cryptocurrency_type=coins",
+      {
+        method: "get",
+        headers: new Headers({
+          Accept: "application/json",
+          "X-CMC_PRO_API_KEY": "776f4a5a-85ec-40de-9a6d-f99b9dc94e22",
+        }),
+      }
+    );
+    const data = await resp.json();
+    setData(data.data);
+    setLoading(false);
+  };
+
+  const renderItem = ({ item }) => <Item title={item.name} />;
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.cryptoList}>
-        <FlatList
-          data={DATA}
-          renderItem={({item}) =>
-          <CryptoItem item={item}></CryptoItem>
-      }
-          keyExtractor={item => item.id}
-        />
+        {data && (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <CryptoItem item={item} callback={selectCrypto}></CryptoItem>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -50,16 +106,17 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight || 0,
   },
   cryptoList: {
+    paddingTop: 10,
     borderRadius: 7,
-    borderStyle: 'solid',
+    borderStyle: "solid",
     borderBottomWidth: 1,
     borderColor: "#A5A5A5",
-    overflow: 'scroll',
+    overflow: "scroll",
     margin: 8,
-    maxHeight: 130
+    maxHeight: 140,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
--- components/CryptoItem.js
+++ components/CryptoItem.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export default function CryptoItem({item}) {
-
-    return (
-        <TouchableOpacity>
-            <Text style={styles.item}>{item.title}</Text>
-        </TouchableOpacity>
-    )
+export default function CryptoItem({ item, callback }) {
+  return (
+    <TouchableOpacity onPress={() => callback(item.name)}>
+      <Text style={styles.item}>
+        <Text>{item.name}</Text>
+        <Text> hyjhfgb </Text>
+      </Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 16,
-        marginTop: 16,
-        borderColor: '#bbb',
-        borderWidth: 1,
-
-    }
-})
+  item: {
+    padding: 16,
+    justifyContent: "space-around",
+    marginTop: 16,
+    display: "flex",
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderRadius: 2,
+  },
+});
