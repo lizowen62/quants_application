c65c0a902b1afacbb5f71c0c3bb40b57299eab76
gabriel <motta.gabriel51@gmail.com>
2022-02-24 19:43:17 +0100

    adding websocket

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Text,
   StatusBar,
 } from "react-native";
+import { VictoryBar,VictoryCandlestick, VictoryAxis, VictoryChart, VictoryLine, VictoryTheme } from "victory-native";
 import {
   backgroundColor,
   borderColor,
@@ -15,19 +16,26 @@ import CryptoItem from "./components/CryptoItem";
 
 import WS from "react-native-websocket";
 
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
+const data2 = [
+  { year: '2011', earnings: 13000 },
+  { year: '2012', earnings: 16500 },
+  { year: '2013', earnings: 14250 },
+  { year: '2014', earnings: 19000 }
+ ];
+
+ const sampleDataDates = [
+  { x: new Date(2016, 6, 1), open: 5, close: 10, high: 15, low: 0 },
+  { x: new Date(2016, 6, 2), open: 10, close: 15, high: 20, low: 5 },
+  { x: new Date(2016, 6, 3), open: 15, close: 20, high: 22, low: 10 },
+  { x: new Date(2016, 6, 4), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 5), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 6), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 7), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 8), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 9), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 10), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 11), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 5), open: 10, close: 8, high: 15, low: 5 }
 ];
 
 const Item = ({ title }) => (
@@ -39,11 +47,12 @@ const Item = ({ title }) => (
 const App = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [select, setSelect] = useState("");
 
   const [crypto, setCrypto] = useState([
     {
       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-      title: "First Item",
+      title: "Firctst Item",
     },
     {
       id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
@@ -51,7 +60,7 @@ const App = () => {
     },
     {
       id: "58694a0f-3da1-471f-bd96-145571e29d72",
-      title: "Thirddyuou Itiem",
+      title: "Thirddyuoru Itiem",
     },
   ]);
 
@@ -60,14 +69,15 @@ const App = () => {
   }, []);
 
   const selectCrypto = (key) => {
-    setCrypto((prev) => {
-      return prev.filter((todo) => todo.title != key);
+    setSelect(key)
+    setData((prev) => {
+      return prev.filter((todo) => todo.name != key);
     });
   };
 
   const fetchData = async () => {
     const resp = await fetch(
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=200&cryptocurrency_type=coins",
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=20&cryptocurrency_type=coins",
       {
         method: "get",
         headers: new Headers({
@@ -96,6 +106,22 @@ const App = () => {
           />
         )}
       </View>
+      <View style={styles.graph}>
+      <VictoryChart 
+      width={350}
+      theme={VictoryTheme.material}
+      domainPadding={{ x: 40 }}
+      scale={{ x: "time" }}
+      >
+              <VictoryAxis tickFormat={t => `${t.getDate()}/${t.getMonth()}`} />
+        <VictoryAxis dependentAxis />
+        <VictoryCandlestick
+          candleColors={{ positive: "#5f5c5b", negative: "#c43a31" }}
+          data={sampleDataDates}
+        />
+      </VictoryChart>
+      </View>
+      { select.length != 0 ? (<Text>Cours Actuel du { select }</Text> ) : (<Text></Text>)}
     </SafeAreaView>
   );
 };
@@ -105,6 +131,12 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
   },
+  graph: {
+    padding: 8,
+    paddingLeft: 30,
+    justifyContent:'center',
+    alignItems: 'center'
+  },
   cryptoList: {
     paddingTop: 10,
     borderRadius: 7,
@@ -113,7 +145,7 @@ const styles = StyleSheet.create({
     borderColor: "#A5A5A5",
     overflow: "scroll",
     margin: 8,
-    maxHeight: 140,
+    maxHeight: 180,
   },
   item: {
     backgroundColor: "#f9c2ff",
--- components/CryptoItem.js
+++ components/CryptoItem.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View,  Text, TouchableOpacity } from "react-native";
 
 export default function CryptoItem({ item, callback }) {
   return (
     <TouchableOpacity onPress={() => callback(item.name)}>
-      <Text style={styles.item}>
+      <View style={styles.item}>
         <Text>{item.name}</Text>
-        <Text> hyjhfgb </Text>
-      </Text>
+        <Text style={styles.tag}>{item.symbol}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -15,11 +15,18 @@ export default function CryptoItem({ item, callback }) {
 const styles = StyleSheet.create({
   item: {
     padding: 16,
-    justifyContent: "space-around",
     marginTop: 16,
-    display: "flex",
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     borderColor: "#bbb",
     borderWidth: 1,
     borderRadius: 2,
   },
+  tag: {
+      padding: 4,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderRadius: 2,
+  }
 });
